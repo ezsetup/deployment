6a08488e53f6238f069804f511ee26199596039b
Dung Viet Nguyen <dvnguyen.vn@gmail.com>
2018-03-28 11:30:43 -0500

    Update deployment scripts

--- api/build.sh
+++ api/build.sh
@@ -1,11 +1,11 @@
 cd ..
 source .env
-git -C source pull || git clone https://github.com/nguyendv/ezsetup source
+git -C source pull || git clone https://github.com/ezsetup/ezsetup source
 
 cd api
 rsync -av ../source/api/ ./app
 
-docker build -t ezsetup/api:${API_SERVER_VERSION} -t ezsetup/api:latest .
+docker build -t ezsetup/api:${API_VERSION} -t ezsetup/api:latest .
 
-docker push ezsetup/api:${API_SERVER_VERSION}
+docker push ezsetup/api:${API_VERSION}
 docker push ezsetup/api:latest
--- frontend/build.sh
+++ frontend/build.sh
@@ -1,10 +1,10 @@
 cd ..
 source .env
-git -C source pull || git clone https://github.com/nguyendv/ezsetup source
+git -C source pull || git clone https://github.com/ezsetup/ezsetup source
 cd source/frontend \
     && npm install && API_SERVER=http://172.16.100.4:5002 npm run build
 cd ../../frontend \
     && cp -r ../source/frontend/dist/ ./dist/
-docker build -t ezsetup/frontend:${WEBAPP_VUE_VERSION} -t ezsetup/frontend:latest .
-docker push ezsetup/frontend:${WEBAPP_VUE_VERSION}
+docker build -t ezsetup/frontend:${FRONTEND_VERSION} -t ezsetup/frontend:latest .
+docker push ezsetup/frontend:${FRONTEND_VERSION}
 docker push ezsetup/frontend:latest
