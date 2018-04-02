0042ed3da1efbf3da9f7dfaa3344c71a3db59014
Dung Viet Nguyen <dvnguyen.vn@gmail.com>
2018-04-02 01:41:13 -0500

    Update

--- frontend/build.sh
+++ frontend/build.sh
@@ -2,7 +2,7 @@ cd ..
 source .env
 git -C source pull || git clone https://github.com/ezsetup/ezsetup source
 cd source/frontend \
-    && npm install && API_SERVER=http://172.16.100.4:5002 npm run build
+    && npm install && npm run build
 cd ../../frontend \
     && cp -r ../source/frontend/dist/ ./dist/
 docker build -t ezsetup/frontend:${FRONTEND_VERSION} -t ezsetup/frontend:latest .
